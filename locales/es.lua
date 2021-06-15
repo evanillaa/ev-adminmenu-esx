ff21cf5d3f6c6a2e16f31ae2a7f75a27521e87f4
Bombay <72895600+BombayV@users.noreply.github.com>
2021-06-15 01:21:20 -0400

    Feat: Added english translations for spanish

--- locales/es.lua
+++ locales/es.lua
@@ -1,3 +1,63 @@
-i18n.importData("es",{
-    welcome_message = "a"
+i18n.importData("es", {
+  -- Top Menu
+  admin_menu = "Admin menu",
+
+  -- Menu labels
+  admin_options_label = "Admin options",
+  player_options_label = "Personal options",
+  vehicle_options_label = "Vehicle options",
+  server_options_label = "Server options",
+
+  -- Server options
+  delete_all_label = "Delete all",
+  delete_all_options_label = "Choose veh/peds/obj/chat/all",
+  delete_all_question_label = "Would you like to delete obj too (yes/no)?",
+  send_announcement_label = "Send an annnouncement",
+  announcement_title_label = "Title of Announcement",
+  announcement_message_label = "Message of Announcement",
+  announcement_time_label = "Length of announcement (sec)",
+  kick_all_label = "Kick all",
+  kick_all_label_message = "Input kick message (optional)",
+  freeze_all_label = "Freeze all",
+  bring_all_label = "Bring all",
+  revive_all_label = "Revive all",
+  freeze_zone_label = "Freeze a zone",
+  freeze_zone_x_label = "Input x value",
+  freeze_zone_ask_label = "Unfreeze zone? (yes/no)",
+  freeze_zone_question_label = "Negative value? (yes/no)",
+  freeze_zone_y_label = "Input y value",
+  freeze_zone_z_label = "Input z value",
+  freeze_zone_radius_label = "Input radius value",
+
+  -- Admin Options
+  no_clip_label = "NoClip",
+  invincible_label = "Invincible",
+  invisible_label = "Invisible",
+  healArmor_label = "Heal/Armor",
+  healArmor_menu_label = "Choose H/S (optional)",
+  revive_label = "Revive",
+  marker_label = "Teleport to Marker",
+  ped_label = "Change your model",
+  ped_spawn_menu_label = "Input the model name or reset",
+  
+  -- Vehicle options
+  spawn_vehicle_label = "Spawn vehicle",
+  spawn_vehicle_model_label = "Choose the model of the vehicle",
+  fix_vehicle_label = "Fix vehicle",
+  delete_vehicle_label = "Delete vehicle",
+  find_vehicle_label = "Teleport to closest vehicle",
+  plate_vehicle_label = "Change vehicle plate",
+  plate_changer_label = "Input new plate text",
+
+  -- Target options
+  revive_player_label = "Revive player",
+  kill_player_label = "Kill player",
+  freeze_player_label = "Freeze player",
+  give_vehicle_player_label = "Give player a vehicle",
+  teleport_label = "Go to player",
+  bring_label = "Bring player",
+  kick_label = "Kick player",
+  kick_player_label = "Input kick message (optional)",
+  giveweapon_label = "Give player a weapon",
+  get_info_label = "Get information"
 })
